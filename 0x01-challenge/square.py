7ed1386752886fb2db341e6f063a40527f07891a
rabiebn <rabie.bnmr@gmail.com>
2024-02-11 18:45:13 +0100

    1: Restore the initial fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-"""Module"""
+""" Module Docs"""
 
 
-class square():
-    """ Class Square """
+class Square():
+    """ Class Docs"""
     width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Initialize """
+        """ Function Docs """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """ Area Docs"""
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """ Calculating Permiter """
-        return self.width * 4
+    def permiter_of_my_square(self):
+        """ Perimeter Docs """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ String format """
-        return "{}/{}".format(self.width, self.width)
+        """ STR DOCS """
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ Another doc :') """
-    s = square(width=12)
+    """ Final DOcs """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
